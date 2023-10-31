be113ad1f0da7c6607425189d5e64f659470ec36
isiagi <isiagigeofrey0@gmail.com>
2023-10-31 13:13:58 +0300

    chore: Adds Header buttons

--- App.js
+++ App.js
@@ -12,6 +12,18 @@ function HomeScreen({ route, navigation }) {
     }
   }, [route.params?.post]);
 
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    // Use `setOptions` to update the button that we previously specified
+    // Now the button includes an `onPress` handler to update the count
+    navigation.setOptions({
+      headerRight: () => (
+        <Button onPress={() => setCount((c) => c + 1)} title="Update count" />
+      ),
+    });
+  }, [navigation]);
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
@@ -29,6 +41,7 @@ function HomeScreen({ route, navigation }) {
         onPress={() => navigation.navigate("CreatePost")}
       />
       <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
+      <Text>Count: {count}</Text>
     </View>
   );
 }
@@ -128,7 +141,13 @@ function App() {
           //     fontWeight: "bold",
           //   },
           // }}
-          options={{ headerTitle: (props) => <LogoTitle {...props} /> }}
+          options={{
+            headerTitle: (props) => <LogoTitle {...props} />,
+            // headerRight: () => (
+            //   <Button onPress={() => alert("This is a button")} title="Info" />
+            // ),
+            headerRight: () => <Button title="Info" />,
+          }}
         />
         <Stack.Screen
           name="Details"
